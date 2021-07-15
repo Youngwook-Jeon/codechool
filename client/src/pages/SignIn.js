e82f127be186c2af4522733e51199d6b711023b2
Young wook Jeon <ela87@naver.com>
2021-07-15 14:47:56 +0900

    Add an action to login

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -5,43 +5,45 @@ import { Link } from "react-router-dom";
 import SignInForm from "../components/forms/SignInForm";
 import validator from "validator";
 import { isObjEmpty } from "../helpers/helpers";
+import { loginUser } from "../actions/authActions";
 
 const SignIn = () => {
-    const [errors, setErrors] = useState({});
-    const dispatch = useDispatch();
+  const [errors, setErrors] = useState({});
+  const dispatch = useDispatch();
 
-    useEffect(() => {
+  useEffect(() => {});
 
-    });
+  const login = ({ email, password }) => {
+    const errors = {};
+    setErrors(errors);
 
-    const login = ({ email, password }) => {
-        const errors = {};
-        setErrors(errors);
-
-        if (!validator.isEmail(email)) {
-            errors.email = "유효하지 않은 이메일입니다.";
-        }
-
-        if (validator.isEmpty(password)) {
-            errors.password = "패스워드를 입력하세요.";
-        }
-
-        if (!isObjEmpty(errors)) {
-            setErrors(errors);
-            return;
-        }
-
-
+    if (!validator.isEmail(email)) {
+      errors.email = "유효하지 않은 이메일입니다.";
     }
+
+    if (validator.isEmpty(password)) {
+      errors.password = "패스워드를 입력하세요.";
+    }
+
+    if (!isObjEmpty(errors)) {
+      setErrors(errors);
+      return;
+    }
+
+    dispatch(loginUser({ email, password }))
+      .then((response) => {})
+      .catch((err) => {});
+  };
   return (
     <Container className="mt-5">
       <Row>
         <Col sm="12" md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
           <Card body>
-            <h2>로그인</h2>
+            <h3>로그인</h3>
+            <hr />
             <SignInForm errors={errors} onSubmitCallback={login}></SignInForm>
             <div className="mt-4">
-                <Link to={"/signup"}>계정이 없으신가요? 가입하러 가기</Link>
+              <Link to={"/signup"}>계정이 없으신가요? 가입하러 가기</Link>
             </div>
           </Card>
         </Col>
