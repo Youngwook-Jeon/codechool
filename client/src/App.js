6125303c2a86e4e07865a7a9a860f6dbcb890cb0
Young wook Jeon <ela87@naver.com>
2021-07-15 13:08:03 +0900

    Configuring redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./layouts/Navigation";
+import Posts from "./pages/Posts";
+import SignIn from "./pages/SignIn";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div>
-      <Navigation />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Navigation />
+        </div>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Posts}></Route>
+            <Route exact path="/signin" component={SignIn}></Route>
+          </Switch>
+        </Container>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/layouts/Navigation.js
+++ client/src/layouts/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand>Codechool</Navbar.Brand>
+      <Navbar.Brand as={NavLink} to={'/'}>Codechool</Navbar.Brand>
       <Navbar.Toggle aria-controls="main-menu"></Navbar.Toggle>
       <Navbar.Collapse id="main-menu">
         <Nav className="mr-auto">
@@ -12,6 +13,7 @@ const Navigation = () => {
         </Nav>
         <Nav>
           <Nav.Link>가입하기</Nav.Link>
+          <Nav.Link as={NavLink} to={'/signin'}>로그인</Nav.Link>
           <NavDropdown title="Mayer Jeon" id="menu-dropdown">
             <NavDropdown.Item>포스트</NavDropdown.Item>
             <NavDropdown.Item>로그아웃</NavDropdown.Item>
