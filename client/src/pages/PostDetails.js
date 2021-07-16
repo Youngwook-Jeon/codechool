31aa562a13642fc67cca7c8f1a9f94bc8a88524d
Young wook Jeon <ela87@naver.com>
2021-07-16 13:16:14 +0900

    Use react-syntax-highlighter to highlight our content of a post

--- client/src/pages/PostDetails.js
+++ client/src/pages/PostDetails.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import { POST_DETAILS_ENDPOINT } from "../helpers/endpoints";
+import { Card, Jumbotron, Button } from "react-bootstrap";
+import moment from "moment";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 const PostDetails = () => {
   const { id } = useParams();
@@ -20,7 +23,39 @@ const PostDetails = () => {
       });
   }, []);
 
-  return <div>post details</div>;
+  return (
+    <div className="pb-4">
+      {post && (
+        <React.Fragment>
+          <Jumbotron>
+            <h1>{post.title}</h1>
+            <p>
+              작성자: {post.user.firstName}, {moment(post.createdAt).fromNow()}
+            </p>
+          </Jumbotron>
+
+          <Card>
+            <Card.Header>
+              <Button
+                className="mr-2"
+                variant="primary"
+                size="sm"
+                onClick={() => {}}
+              >
+                다운로드
+              </Button>
+              <Button variant="primary" size="sm" onClick={() => {}}>
+                클립보드에 복사
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <SyntaxHighlighter showLineNumbers={true}>{post.content}</SyntaxHighlighter>
+            </Card.Body>
+          </Card>
+        </React.Fragment>
+      )}
+    </div>
+  );
 };
 
 export default PostDetails;
